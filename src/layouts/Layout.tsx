2d0ad7f203d1aca4dee2383755fb1b2c9b50cc1a
Vitaliy Kornatskiy <kvi@exante.eu>
2023-06-08 21:17:46 +0300

    fix

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -22,6 +22,8 @@ const Wrapper = styled.div`
 export const Layout = () => {
   const { wallet } = useAuth();
   const userSocket = useUserSocket();
+  
+  console.log(wallet);
 
   if (!wallet) {
     return (
