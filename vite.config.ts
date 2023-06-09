1bf639c50fb2bcfabcd110812853d1e6575df4be
Vitaliy Kornatskiy <kvi@exante.eu>
2023-06-09 18:56:27 +0300

    removed unused deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
 import path from "path";
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    wasm(),
-    topLevelAwait(),
     react(),
     viteTsconfigPaths(),
     svgrPlugin(),
